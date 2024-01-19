60533cb69c4fbed32ea1942672007a17748b5677
Ronaldo-spec <ronaldofirmansyah.30@gmail.com>
2024-01-19 14:30:15 +0700

    inter-5

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 
 const Hapi = require("@hapi/hapi");
-const songs = require("./api/songs");
-const albums = require("./api/albums");
-const SongsService = require('./services/postgres/SongsService');
-const AlbumsService = require('./services/postgres/AlbumsService');
-const SongsValidator = require('./validator/songs');
-const AlbumsValidator = require('./validator/albums');
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
+const NotesValidator = require('./validator/notes');
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
-  const songsService = new SongsService();
-  const albumsService = new AlbumsService(); 
+  const notesService = new NotesService();
   const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     port: process.env.PORT,
     host: process.env.HOST,
     routes: {
@@ -22,41 +20,30 @@ const init = async () => {
     },
   });
   console.log(process.env.PORT);
- 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
-    },
-  });
 
   await server.register({
-    plugin: albums,
+    plugin: notes,
     options: {
-      service: albumsService,
-      validator: AlbumsValidator,
+      service: notesService,
+      validator: NotesValidator,
     },
   });
-
-
-  // server.ext('onPreResponse', (request, h) => {
-  //   // mendapatkan konteks response dari request
-  //   const { response } = request;
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
   
-  //   // penanganan client error secara internal.
-  //   if (response instanceof ClientError) {
-  //     const newResponse = h.response({
-  //       status: 'fail',
-  //       message: response.message,
-  //     });
-  //     newResponse.code(response.statusCode);
-  //     return newResponse;
-  //   }
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
       
-  //   return h.continue;
-  // });
-
+    return h.continue;
+  });
  
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,9 @@
-const mapDBToModel = ({
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
   id,
   title,
-  year,
-  performer,
-  genre,
-  duration,
-  inserted_at,
-  updated_at,
-}) => ({
-  id,
-  title,
-  year,
-  performer,
-  genre,
-  duration,
-  insertedAt: inserted_at,
+  body,
+  tags,
+  createdAt: created_at,
   updatedAt: updated_at,
 });
 
